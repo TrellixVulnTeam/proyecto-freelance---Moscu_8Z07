0dae62d3db0c5ae048fbc6b316f2904524df4a8b
Emanuel Valente <ema_zero@hotmail.com>
2022-06-09 02:33:33 -0300

    09-06-22: Animacion de brazo terminada

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// coding gsap library for hand animation.
+gsap.to('.handMoving', {
+    duration: 2,
+    y: '-100%',
+    opacity: 1
+});
+
+let tl = gsap.timeline({
+    delay: 2,
+    repeat: -1,
+    yoyo: true,
+    defaults: { duration: 1 }
+})
+
+tl.to('.handMoving', {
+    rotation: -10
+})
+
+    .to('.handMoving', {
+        rotation: 10
+    });
+
+
+
